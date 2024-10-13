b9cad2882a6b6fa62d74fa70d7a10feb9e36fd33
Muhammad Rahmat <muhammad.rahmat@wartek.belajar.id>
2024-10-14 01:05:47 +0800

    add .env

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,9 @@
-const APP_URL = 'https://simanis.test';
-const API_PREFIX = '/api';
+// const APP_URL = 'https://simanis.test';
+// const API_PREFIX = '/api';
 
-export const API_URL: string = APP_URL + API_PREFIX;
+// export const API_URL: string = APP_URL + API_PREFIX;
+
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-extraneous-dependencies
+import { API_PREFIX, APP_URL } from 'react-native-dotenv';
+
+export const API_URL: string = `${APP_URL}${API_PREFIX}`;
---
+++ env.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-native-dotenv' {
+  export const API_PREFIX: string;
+  export const APP_URL: string;
+}
